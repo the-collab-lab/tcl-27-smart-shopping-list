50400170c907e2ceeaf1b25cd17b6cac0d07d0cd
Stacie Taylor <stacietaylorcima@gmail.com>
2021-07-04 15:00:01 -0700

    Update firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,16 +4,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 // Initalize Firebase.
-// These details will need to be replaced with the project specific env vars at the start of each new cohort.
-var firebaseConfig = {
-  apiKey: { TCL_X_API_KEY },
-  authDomain: { TCL_X_AUTH_DOMAIN },
-  databaseURL: { TCL_X_DATABASE_URL },
-  projectId: { TCL_X_PROJECT_ID },
-  storageBucket: { TCL_X_STORAGE_BUCKET },
-  messagingSenderId: { TCL_X_SENDER_ID },
-  appId: { TCL_X_APP_ID },
-};
+  var firebaseConfig = {
+    apiKey: "AIzaSyB9-K-FKVH2s-dGBLKpXKX-kfE4hENprak",
+    authDomain: "tcl-27-shopping-list.firebaseapp.com",
+    projectId: "tcl-27-shopping-list",
+    storageBucket: "tcl-27-shopping-list.appspot.com",
+    messagingSenderId: "467906502054",
+    appId: "1:467906502054:web:dd45ccdf5ec94fd2f54367"
+  };
 
 let fb = firebase.initializeApp(firebaseConfig);
 
