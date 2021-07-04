418c34f4f814a632f43d6c5f376256a2f8050ec6
Stacie Taylor <stacietaylorcima@gmail.com>
2021-07-04 15:10:34 -0700

    Oops reformatting that firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,14 +4,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 // Initalize Firebase.
-  var firebaseConfig = {
-    apiKey: "AIzaSyB9-K-FKVH2s-dGBLKpXKX-kfE4hENprak",
-    authDomain: "tcl-27-shopping-list.firebaseapp.com",
-    projectId: "tcl-27-shopping-list",
-    storageBucket: "tcl-27-shopping-list.appspot.com",
-    messagingSenderId: "467906502054",
-    appId: "1:467906502054:web:dd45ccdf5ec94fd2f54367"
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyB9-K-FKVH2s-dGBLKpXKX-kfE4hENprak",
+  authDomain: "tcl-27-shopping-list.firebaseapp.com",
+  projectId: "tcl-27-shopping-list",
+  storageBucket: "tcl-27-shopping-list.appspot.com",
+  messagingSenderId: "467906502054",
+  appId: "1:467906502054:web:dd45ccdf5ec94fd2f54367"
+};
 
 let fb = firebase.initializeApp(firebaseConfig);
 
