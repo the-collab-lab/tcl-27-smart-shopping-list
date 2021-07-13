bf145e0ab83b0032e1dbd14511484254f5ee7df6
airzt11 <airzonatea@gmail.com>
2021-07-13 00:00:52 -0400

    figured out GET and POST functionality with firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import firebase from 'firebase/app';
+import { fb } from './lib/firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import 'firebase/firestore';
+// running into error with importing GroceryComponent
+// import { GroceryComponent } from './components/GroceryComponent';
 
 function App() {
+  const [grocery, setGrocery] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const ref = fb.firestore().collection('groceries');
+
+  useEffect(() => {
+    getGroceries();
+  }, []);
+
+  const getGroceries = () => {
+    ref.onSnapshot((querySnapshot) => {
+      const groceries = [];
+      querySnapshot.forEach((item) => {
+        groceries.push(item.data());
+      });
+      setGrocery(groceries);
+    });
+  };
+
+  const postItem = (item) => {
+    ref
+      .doc(item.id)
+      .set(item)
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  console.log(grocery);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      TEST GROCERIES
+      {/* <form>
+        <input onChange={handleChange} ></input>
+      </form> */}
+      <br />
+      <button onClick={() => postItem({ item: 'bananas', quantity: 10 })}>
+        submit an item...
+      </button>
+      <ul>
+        {grocery.map((g) => (
+          <li key={g.id}>
+            Item: {g.item}
+            <br />
+            Quantity: {g.quantity}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/components/GroceryComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GroceryComponent = () => {
+  return <div>GROCERIES</div>;
+};
+
+export default GroceryComponent;
