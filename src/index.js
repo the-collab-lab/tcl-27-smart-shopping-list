0eead1645ab385a689ed89a0c3536f117b980102
airzt11 <airzonatea@gmail.com>
2021-07-16 19:00:30 -0400

    Move app.js to src and consolidate components folder
    Updated dependencies and consolidated code from duplicate app.js files
    into one app.js file and moved it to the src folder. Also consolidated
    duplicate components/containers folders.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import App from './containers/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
