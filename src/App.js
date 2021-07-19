e07f28d36e0788db47cb69e4bb3cc313b2616943
airzt11 <airzonatea@gmail.com>
2021-07-19 19:50:13 -0400

    Generate unique token and redirect to /list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import firebase from 'firebase/app';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import 'firebase/firestore';
+import getToken from './lib/tokens';
+
 import GroceryContainer from './components/containers/GroceryContainer';
 import List from './components/containers/List';
 import AddItem from './components/AddItem';
 import BottomNav from './components/BottomNav';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      return setLoggedIn(true);
+    }
+  });
+
+  const handleClick = () => {
+    const token = getToken();
+    localStorage.setItem('token', token);
+    setLoggedIn(true);
+  };
+
   return (
     <div className="App">
       <GroceryContainer />
       <BottomNav />
       <Switch>
+        <Route exact path="/">
+          {loggedIn ? <Redirect to="/list" /> : null}
+        </Route>
+
         <Route path="/list" component={List}></Route>
         <Route path="/add-an-item" component={AddItem}></Route>
       </Switch>
+
+      <button onClick={handleClick}>Create List...</button>
     </div>
   );
 }
