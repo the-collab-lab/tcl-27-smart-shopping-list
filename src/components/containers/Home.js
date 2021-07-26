a7cdb5b516e028a11c636d5c24c39d7e599ea741
dpoplin <danih37@gmail.com>
2021-07-26 18:54:31 -0500

    Create form for adding toekn
    Co-authored-by: Dani Poplin <danih37@gmail.com>

--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: '',
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      token: e.target.value,
+    });
+  };
+
+  handleClick = (e) => {
+    e.preventDefault();
+    console.log(this.state.token);
+  };
+
   render() {
     return (
       <div>
-        <h1>Home</h1>
+        <label htmlFor="token">Enter token:</label>
+        <input
+          type="text"
+          id="token"
+          onChange={this.handleChange}
+          value={this.state.token}
+        />
+        <button
+          type="submit"
+          name="submit"
+          value="submit"
+          onClick={(e) => this.handleClick(e)}
+        >
+          Submit
+        </button>
       </div>
     );
   }
