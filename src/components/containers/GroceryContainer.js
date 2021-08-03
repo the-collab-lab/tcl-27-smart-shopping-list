1873a366ead64d4b60047bd30aa3b83647dff832
tscotce <tatyana.celovsky@gmail.com>
2021-08-03 18:13:55 -0400

    Add welcome prompt and a button to redirect to Add an Item view
    Co-authored-by: Yolanda Haynes <yhaynes@giyo.us>

--- src/components/containers/GroceryContainer.js
+++ src/components/containers/GroceryContainer.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { fb } from '../../lib/firebase';
 import GroceryCard from '../GroceryCard';
+import { useHistory } from 'react-router-dom';
 
 const GroceryContainer = () => {
   const [grocery, setGrocery] = useState([]);
+  const history = useHistory();
 
   const ref = fb
     .firestore()
@@ -31,18 +33,34 @@ const GroceryContainer = () => {
     });
   };
 
-  return (
-    <div>
-      GroceryContainer
-      <ul>
-        {grocery.map((g) => (
-          <li key={g.itemName}>
-            <GroceryCard item={g} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  const handleClick = () => {
+    history.push('/add-an-item');
+  };
+
+  if (grocery.length === 0) {
+    return (
+      <div>
+        <h1>Smart Shopping List</h1>
+        <p>Your shopping list is currently empty.</p>
+        <button type="submit" onClick={handleClick}>
+          Add an item
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Smart Shopping List</h1>
+        <ul>
+          {grocery.map((g) => (
+            <li key={g.itemName}>
+              <GroceryCard item={g} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default GroceryContainer;
