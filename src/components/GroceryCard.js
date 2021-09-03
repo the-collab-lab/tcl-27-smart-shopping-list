0c01c5eef2303bad73694310b940c8bb222689da
airzt11 <airzonatea@gmail.com>
2021-09-02 22:12:43 -0400

    Import accordion differently

--- src/components/GroceryCard.js
+++ src/components/GroceryCard.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import React, { useEffect, useState } from 'react';
 import calculateEstimate from '../lib/estimates';
 import { fb } from '../lib/firebase';
-import Accordion from 'react-bootstrap/Accordion';
+import { Accordion } from 'react-bootstrap';
 import MaterialIcon, {
   circle,
   remove_circle,
--- src/components/UserToken.js
+++ src/components/UserToken.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Accordion from 'react-bootstrap/Accordion';
+import { Accordion } from 'react-bootstrap';
 
 const UserToken = () => {
   return (
